167a9ac798b1fc583c7824b73108ba8a64c87264
Andreh Silveira <andrehbahia@gmail.com>
2023-05-24 18:44:35 -0300

    solicitaPeso

--- src/App.java
+++ src/App.java
@@ -4,7 +4,13 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         int peso1, peso2, peso3, pesoMax;
-        pesoMax = 301;
+        System.out.println(" Digite o peso da primeira pessoa: ");
+        peso1 = sc.nextInt();
+        System.out.println(" Digite o peso da segundo pessoa: ");
+        peso2 = sc.nextInt();
+        System.out.println(" Digite opeso da terceira pessoa: ");
+        peso3 = sc.nextInt();
+        pesoMax = peso1 +peso2 +peso3;
         while ( pesoMax > 300 ) {
           System.out.println(" Elevador não pode subir, risco de queda, por favor saiam todos. ");
           peso1 = sc.nextInt();
